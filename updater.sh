6429811e969144f443f2f11aef293b9189f51a4c
Abdu Zoghbi <astrozoghbi@gmail.com>
2025-07-12 16:17:11 -0400

    update updater script

--- updater.sh
+++ updater.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
-set -e
-set -o pipefail
 
-# download the files
-update_from=6.34
-update_to=6.35
+# globale variables.
+CURRENT_VERSION=6.34
+TARGET_VERSION=6.35
 
-hea_url="https://heasarc.gsfc.nasa.gov/FTP/software/lheasoft"
+HEA_URL="https://heasarc.gsfc.nasa.gov/FTP/software/lheasoft"
+FILES=("xsChain.cxx" "Chain.h" "Chain.cxx" "ChainManager.h" "ChainManager.cxx")
 
-# download the files for the current version
-mkdir -p $update_from $update_to
 
 get_path() {
     local fname=$1
@@ -20,17 +17,17 @@ get_path() {
     fi
 }
 
-# Generate patches for the current version if they don't exist
-rm *tmp* > /dev/null 2>&1 || true
-if [ $(ls *.*.patch | wc -l | awk '{print $1}') -ne 5 ]; then
-    echo "Generating patches for the current version..."
+generate_patches() {
+    # generate patches for from modified files. Before 6.35, we had the modified
+    # files in the repository, this generates patches from them.
+    echo "****\nGenerating patches for the current version...\n***\n"
+    local update_from=$1
     for file in `find . -maxdepth 1 -name '*.cxx' -o -name '*.h'`; do
         fname=$(basename $file)
         path=$(get_path $fname)
+        echo "Found $fname"
         
-        if [ ! -f $update_from/$fname ]; then
-            curl "$hea_url/lheasoft${update_from}/src/$path/$fname" -o $update_from/$fname
-        fi
+        curl "$hea_url/lheasoft${update_from}/src/$path/$fname" -o $update_from/$fname
         diff -u $update_from/$fname $fname > $fname.patch
         # replace top two lines with name only
         echo "--- $fname" > $fname.patch.tmp
@@ -38,31 +35,45 @@ if [ $(ls *.*.patch | wc -l | awk '{print $1}') -ne 5 ]; then
         tail -n +3 $fname.patch >> $fname.patch.tmp
         mv $fname.patch.tmp $fname.patch
     done
-else
-    echo "Patches already exist, skipping patch generation."
-    unpatched=""
-    for file in `find . -name '*.patch'`; do
-        fname=$(echo $file | sed 's|\.patch$||' | sed 's|./||')
+}
+
+download_version() {
+    # Given a file name in FILES, return the path in Xspec source
+    local version=$1
+    mkdir -p $version
+    echo "Downloading files for version $version..."
+    for fname in ${FILES[@]}; do
         path=$(get_path $fname)
-        curl -s "$hea_url/lheasoft${update_from}/src/$path/$fname" -o $update_from/$fname
-        curl -s "$hea_url/lheasoft${update_to}/src/$path/$fname" -o $update_to/$fname
+        if [ ! -f "$version/$fname" ]; then
+            curl -s "$HEA_URL/lheasoft${version}/src/$path/$fname" -o $version/$fname
+        fi
+    done
+}
+
+apply_patches() {
+    # apply patch to a new version
+    local update_from=$1
+    local update_to=$2
+    for fname in ${FILES[@]}; do
+        path=$(get_path $fname)
         diff -u $update_from/$fname $update_to/$fname > $fname.diff
-        
+
         if [ $(wc -l $fname.diff | awk '{print $1}') -gt 0 ]; then
-            echo "+++++ Changes found in $fname"
+            printf "%.20s | CHANGED $update_from to $update_to\n" $fname
             unpatched="$unpatched $fname"
         else
-            echo "No changes in $fname between $update_from and $update_to"
+            printf "%.20s | no change between $update_from to $update_to\n" $fname
             cat $fname.patch | sed "s|$fname|$update_to/$fname|g" > tmp.patch
+            echo "    apply patch for $fname"
             patch -b < tmp.patch
             rm tmp.patch $fname.diff
-            # now get the new patch file
-            # replace top two lines with name only
-            echo "--- $fname" > $fname.patch
-            echo "+++ $fname" >> $fname.patch
-            diff -u $update_to/$fname.orig $update_to/$fname | tail -n +3 >> $fname.patch
         fi
     done
     echo "+++++++++++++++++++"
     echo "Unpatched files: $unpatched"
-fi
+}
+
+# Start of the script
+download_version $CURRENT_VERSION
+download_version $TARGET_VERSION
+apply_patches $CURRENT_VERSION $TARGET_VERSION
